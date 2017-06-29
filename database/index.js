9dc52086cae136f45725672a598c74aaf65cb486
@jamgorilla <jamesmurphy507@hotmail.com>
2017-06-29 14:47:38 +0100

    play button rigged up

--- database/index.js
+++ database/index.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 // for development only: not for deployment
 // comment out before pull request:
-// var secret = require('../secret.js');
+ var secret = require('../secret.js');
 
 // choose between env variables for Heroku or dev env
 var mongodb_user = process.env.MONGODB_USER || secret.MONGODB_USER;
@@ -45,7 +45,7 @@ var selectAll = function(callback) {
 
 // get a list of pins within 1 mile:
 // (lng and lat are the longitude and latitude of the current location of a user)
-var getPinsWithinRadius = function(lng, lat) {
+var getPinsWithinRadius = function(lng, lat, callback) {
   var milesToRadian = function(miles) {
     var earthRadiusInMiles = 3959;
     return miles / earthRadiusInMiles;
@@ -61,38 +61,38 @@ var getPinsWithinRadius = function(lng, lat) {
     if (err) {
       console.error(err);
     } else {
-      console.log('pins within 1 mile:', pins);
-
+      //console.log('pins within 1 mile:', pins);
+      callback(null, pins);
     }
   })
 }
-// getPinsWithinRadius(-122.407087, 37.783506);
+//getPinsWithinRadius(-122.407087, 37.783506);
 
 // create a new db pin for test:
 // running it creates an entry every time, comment out after creating:
 
-// Pin.create({ location: { type: 'Point', coordinates: [-122.408942, 37.783696] }, playlistUrl: 'https://api.spotify.com/v1/users/wizzler/playlists?offset=0&limit=20', playlistName: 'My Playlist' }, function(err) {
-//   if (err) {
-//     console.error(err);
-//   }
-// })
-//
-// Pin.create({ location: { type: 'Point', coordinates: [-122.406646, 37.784612] }, playlist: 'https://open.spotify.com/user/1299590238/playlist/617wZsy9snEuDw2YV7lIq0', playlistName: 'Cozy' }, function(err) {
-//   if (err) {
-//     console.error(err);
-//   }
-// })
-//
-// Pin.create({ location: { type: 'Point', coordinates: [-122.410124, 37.783251] }, playlist: 'https://open.spotify.com/user/1269933467/playlist/0o7b29K3eZ3BNnZswZ9sAC', playlistName: 'Favourites' }, function(err) {
-//   if (err) {
-//     console.error(err);
-//   }
-// })
-// Pin.create({ location: { type: 'Point', coordinates: [-122.388633, 37.790001] }, playlist: 'https://open.spotify.com/user/1269933467/playlist/0o7b29K3eZ3BNnZswZ9sAC', playlistName: 'Playlist' }, function(err) {
-//   if (err) {
-//     console.error(err);
-//   }
-// })
+Pin.create({ location: { type: 'Point', coordinates: [-122.408942, 37.783696] }, playlistUrl: 'https://api.spotify.com/v1/users/wizzler/playlists?offset=0&limit=20', playlistName: 'My Playlist' }, function(err) {
+  if (err) {
+    console.error(err);
+  }
+})
+
+Pin.create({ location: { type: 'Point', coordinates: [-122.406646, 37.784612] }, playlist: 'https://open.spotify.com/user/1299590238/playlist/617wZsy9snEuDw2YV7lIq0', playlistName: 'Cozy' }, function(err) {
+  if (err) {
+    console.error(err);
+  }
+})
+
+Pin.create({ location: { type: 'Point', coordinates: [-122.410124, 37.783251] }, playlist: 'https://open.spotify.com/user/1269933467/playlist/0o7b29K3eZ3BNnZswZ9sAC', playlistName: 'Favourites' }, function(err) {
+  if (err) {
+    console.error(err);
+  }
+})
+Pin.create({ location: { type: 'Point', coordinates: [-122.388633, 37.790001] }, playlist: 'https://open.spotify.com/user/1269933467/playlist/0o7b29K3eZ3BNnZswZ9sAC', playlistName: 'Playlist' }, function(err) {
+  if (err) {
+    console.error(err);
+  }
+})
 
 module.exports.getPinsWithinRadius = getPinsWithinRadius;
 module.exports.selectAll = selectAll;
--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@
 var express = require('express'); // Express web server framework
 var request = require('request'); // "Request" library
 var bodyParser = require('body-parser');
+var secret = require('../secret.js')
 var app = express();
 
 app.use(bodyParser.json());
@@ -21,6 +22,32 @@ app.get('/pins', function (req, res) {
       res.json(data);
     }
   });
+});
+
+// play button get request for playlist in current location
+var db = require('../database');
+
+app.get('/sendClosestPlaylist', function (req, res) {
+  var params = req.url.slice(21).split('=');
+  var lng = JSON.parse(params[0]);
+  var lat = JSON.parse(params[1]);
+  
+  db.getPinsWithinRadius(lng, lat, function(err, data){
+    var closestPin = [];
+
+    for (var k = 0; k < data.length; k++ ) {
+      // find nearest pin
+      var a = Math.abs(lng - data[k].location.coordinates[0]);  
+      var b = Math.abs(lat - data[k].location.coordinates[1]);
+      var c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+      closestPin.push(c);
+    }
+      var min = Math.min.apply(Math, closestPin);
+      var index = closestPin.indexOf(min);
+
+      // send playlist back to client
+      res.send(data[index].playlistUrl);
+  })
 });
 
 // req.body has to come in the format: { location: { type: 'Point', coordinates: [-122.408942, 37.783696] }, playlistUrl: 'https://api.spotify.com/v1/users/wizzler/playlists?offset=0&limit=20', playlistName: 'My Playlist' }
